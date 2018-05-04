ff464b295634027facb2a74566124e101aec91b9
love1 <love1dbsb77@hotmail.com>
2018-05-03 17:42:31 -0700

    Added GameOver

--- Metris/game.py
+++ Metris/game.py
@@ -130,7 +130,7 @@ def checkGameOver():
     for y in range (0, len(landed[0]) - 20):
         for x in range (0, len(landed)):
             if landed[x][y] != None:
-                gameExit = True
+                gameOver()
             
 def checkCollision():
     global currentBlock
@@ -225,127 +225,181 @@ def drawShadow(blockList, dy):
              [blockList[i].getX(), blockList[i].getY() + dy, BLOCK_SIZE, BLOCK_SIZE])
         pygame.draw.rect(gameDisplay, (255, 255, 255),
              [blockList[i].getX() + 1, blockList[i].getY() + dy + 1, BLOCK_SIZE - 2, BLOCK_SIZE - 2])
+
+def paused():
+    pause = True
+
+    while pause:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    pause = False
         
+        gameDisplay.fill(white, [LEFT_BOUNDARY, 0, WIDTH/3 + 10, HEIGHT])
+        pausedText = font.render("Paused", True, black)
+        gameDisplay.blit(pausedText, (WIDTH/3 + WIDTH/6, HEIGHT/2)) 
+        pygame.display.update()
+        clock.tick(15)    
     
+def gameOver():
+    pause = True
+    global currentBlock
+    global block
+    global landed
+    global score
+
+    while pause:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:
+                    currentBlock = False
+                    block = None
+                    score = 0
+                    for i in range(0, len(landed)):
+                        for j in range(0, len(landed[i])):
+                            landed[i][j] = None
+                    runGame()
+        pausedText = font.render("Game Over", True, black)
+        gameDisplay.blit(pausedText, (WIDTH/3 + WIDTH/6, HEIGHT/2)) 
+        pygame.display.update()
+        clock.tick(15)   
+
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+def runGame():
+    global gameExit
+    global currentBlock
+    global hasMove
+    global dx
+    global dy
+    global block
+    global speed
+    while not gameExit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = True
 
-while not gameExit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameExit = True
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                if pos_x:
-                    dx = -BLOCK_SIZE
-                    # dy = 0
-                    if event.key == pygame.K_DOWN:
-                        speed = 10
-            elif event.key == pygame.K_RIGHT:
-                if pos_x:
-                    dx = BLOCK_SIZE
-                    # dy = 0
-                    if event.key == pygame.K_DOWN:
-                        speed = 10
-            elif event.key == pygame.K_DOWN:
-                dy = BLOCK_SIZE
-                # dx = 0
-                speed = 20
-                
-            elif event.key == pygame.K_UP:
-                if currentBlock:
-                    block.rotateR()
-                    if checkCollisionRotation():
-                        block.rotateL()
-            elif event.key == pygame.K_z:
-                if currentBlock:
-                    block.rotateL()
-                    if checkCollisionRotation():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    if pos_x:
+                        dx = -BLOCK_SIZE
+                        # dy = 0
+                        if event.key == pygame.K_DOWN:
+                            speed = 10
+                elif event.key == pygame.K_RIGHT:
+                    if pos_x:
+                        dx = BLOCK_SIZE
+                        # dy = 0
+                        if event.key == pygame.K_DOWN:
+                            speed = 10
+                elif event.key == pygame.K_DOWN:
+                    dy = BLOCK_SIZE
+                    # dx = 0
+                    speed = 20
+                    
+                elif event.key == pygame.K_UP:
+                    if currentBlock:
                         block.rotateR()
-            elif event.key == pygame.K_SPACE:
-                while (not checkCollision()):
-                    block.setY(block.getY() + BLOCK_SIZE)
-                    pos_y += BLOCK_SIZE
+                        if checkCollisionRotation():
+                            block.rotateL()
+                elif event.key == pygame.K_z:
+                    if currentBlock:
+                        block.rotateL()
+                        if checkCollisionRotation():
+                            block.rotateR()
+                elif event.key == pygame.K_SPACE:
+                    while (not checkCollision()):
+                        block.setY(block.getY() + BLOCK_SIZE)
+                        pos_y += BLOCK_SIZE
 
-                block.setY(block.getY() - BLOCK_SIZE)
-                pos_y -= BLOCK_SIZE
+                    block.setY(block.getY() - BLOCK_SIZE)
+                    pos_y -= BLOCK_SIZE
 
+                    pos_y = tick(pos_y)
+                elif event.key == pygame.K_p:
+                    paused()
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    if dx < 0:
+                        dx = 0
+                    # dy = 0
+                if event.key == pygame.K_RIGHT:
+                    if dx > 0:
+                        dx = 0
+                if event.key == pygame.K_DOWN:
+                    dy = 0
+                    # dx = 0
+                    speed = 10
+            if event.type == TICK:
                 pos_y = tick(pos_y)
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                if dx < 0:
-                    dx = 0
-                # dy = 0
-            if event.key == pygame.K_RIGHT:
-                if dx > 0:
-                    dx = 0
-            if event.key == pygame.K_DOWN:
-                dy = 0
-                # dx = 0
-                speed = 10
-        if event.type == TICK:
-            pos_y = tick(pos_y)
-            
+                
 
-    # drawing bg
-    gameDisplay.fill(white)
-    gameDisplay.fill(black, [0, 0, LEFT_BOUNDARY, HEIGHT])
-    gameDisplay.fill(black, [RIGHT_BOUNDARY, 0, WIDTH - RIGHT_BOUNDARY, HEIGHT])
-    gameDisplay.fill(black, [0, 0, WIDTH, HEIGHT - 20*BLOCK_SIZE])
+        # drawing bg
+        gameDisplay.fill(white)
+        gameDisplay.fill(black, [0, 0, LEFT_BOUNDARY, HEIGHT])
+        gameDisplay.fill(black, [RIGHT_BOUNDARY, 0, WIDTH - RIGHT_BOUNDARY, HEIGHT])
+        gameDisplay.fill(black, [0, 0, WIDTH, HEIGHT - 20*BLOCK_SIZE])
 
-    # drawing block objs
-    if not currentBlock and not gameExit:
-        rand = randint(0, 6)
-        if rand == 0:
-            block = BlockT(INIT_X, INIT_Y, BLOCK_SIZE)
-        elif rand == 1:
-            block = BlockS(INIT_X, INIT_Y, BLOCK_SIZE)
-        elif rand == 2:
-            block = BlockJ(INIT_X, INIT_Y, BLOCK_SIZE)
-        elif rand == 3:
-            block = BlockI(INIT_X, INIT_Y, BLOCK_SIZE)
-        elif rand == 4:
-            block = BlockL(INIT_X, INIT_Y, BLOCK_SIZE)
-        elif rand == 5:
-            block = BlockZ(INIT_X, INIT_Y, BLOCK_SIZE)
-        elif rand == 6:
-            block = BlockO(INIT_X, INIT_Y, BLOCK_SIZE)
-        pos_x = INIT_X
-        pos_y = INIT_Y
-        currentBlock = True
-    difference = getShadowDifference(block.getPerimeter())
-    drawShadow(block.getPerimeter(), difference)
-    for i in range(0, len(landed)):
-        for j in range(0, len(landed[i])):
-            if (landed[i][j] != None):
-                landed[i][j].display(gameDisplay)
-    block.display(gameDisplay)
+        # drawing block objs
+        if not currentBlock and not gameExit:
+            rand = randint(0, 6)
+            if rand == 0:
+                block = BlockT(INIT_X, INIT_Y, BLOCK_SIZE)
+            elif rand == 1:
+                block = BlockS(INIT_X, INIT_Y, BLOCK_SIZE)
+            elif rand == 2:
+                block = BlockJ(INIT_X, INIT_Y, BLOCK_SIZE)
+            elif rand == 3:
+                block = BlockI(INIT_X, INIT_Y, BLOCK_SIZE)
+            elif rand == 4:
+                block = BlockL(INIT_X, INIT_Y, BLOCK_SIZE)
+            elif rand == 5:
+                block = BlockZ(INIT_X, INIT_Y, BLOCK_SIZE)
+            elif rand == 6:
+                block = BlockO(INIT_X, INIT_Y, BLOCK_SIZE)
+            pos_x = INIT_X
+            pos_y = INIT_Y
+            currentBlock = True
+        difference = getShadowDifference(block.getPerimeter())
+        drawShadow(block.getPerimeter(), difference)
+        for i in range(0, len(landed)):
+            for j in range(0, len(landed[i])):
+                if (landed[i][j] != None):
+                    landed[i][j].display(gameDisplay)
+        block.display(gameDisplay)
 
-    # score
-    screen_text = font.render("score: " + str(score), True, white)
-    gameDisplay.blit(screen_text, (RIGHT_BOUNDARY +
-                                   LEFT_BOUNDARY / 10, HEIGHT / 20))
+        # score
+        screen_text = font.render("score: " + str(score), True, white)
+        gameDisplay.blit(screen_text, (RIGHT_BOUNDARY +
+                                    LEFT_BOUNDARY / 10, HEIGHT / 20))
 
-    # collision checking
-    if not hasMove:
-        #clock.tick(10) - movement speed
-        block.setX(pos_x + dx)
-        block.setY(pos_y + dy)
-        if checkCollision():
-            block.setX(pos_x)
-            block.setY(pos_y)
-        else:
-            pos_x += dx
-            pos_y += dy
+        # collision checking
+        if not hasMove:
+            #clock.tick(10) - movement speed
+            block.setX(pos_x + dx)
+            block.setY(pos_y + dy)
+            if checkCollision():
+                block.setX(pos_x)
+                block.setY(pos_y)
+            else:
+                pos_x += dx
+                pos_y += dy
 
-        hasMove = True
+            hasMove = True
 
-    pygame.display.update()
+        pygame.display.update()
 
-    clock.tick(speed)
-    hasMove = False
+        clock.tick(speed)
+        hasMove = False
 
 
-pygame.quit()
-quit()
+    pygame.quit()
+    quit()
+
+runGame()
